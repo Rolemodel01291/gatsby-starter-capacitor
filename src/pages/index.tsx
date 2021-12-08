ef9335db414bb0b71cfdc0ef8642386eaf18fa76
Rolemodel01291 <kopedaniela0@gmail.com>
2021-12-07 18:33:56 -0800

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Toast } from "@capacitor/toast";
+import gatsbyStartercapacitor from "../assets/gatsby-starter-capacitor.png";
+
+export default () => {
+  const displayNativeToast = async () => {
+    await Toast.show({
+      text: "Hello world",
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h1>gatsby-starter-capacitor</h1>
+      <img
+        style={{ width: "100%", maxWidth: 640 }}
+        src={gatsbyStartercapacitor}
+        alt="Gatsby and Capacitor"
+      />
+      <button onClick={displayNativeToast}>Display native toast!</button>
+    </div>
+  );
+};
